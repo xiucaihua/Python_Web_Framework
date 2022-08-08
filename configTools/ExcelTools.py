81f3809b994cee3f9c9c9ca450b0884d2a48d5eb
xiucaihua <1198874993@QQ.com>
2022-08-08 21:13:57 +0800

    修改语法错误

--- configTools/ExcelTools.py
+++ configTools/ExcelTools.py
@@ -8,25 +8,25 @@ excleFilePath="D:\\case.xlsx"
 wb=load_workbook(excleFilePath)
 
 #获取所有工作表的名字
-print "所有的工作表的名字为：",wb.sheetnames
+print ("所有的工作表的名字为：",wb.sheetnames)
 
 # 创建指定工作表对象
 # sheet = wb.get_sheet_by_name('Sheet1')
 sheet = wb['Login']
 
 # 获取指定单元格的值
-print sheet['A1'].value                 #获取第一行第二个单元格
-print sheet.cell(row=1,column=2).value  #获取到第2行，第三列
+print (sheet['A1'].value)                 #获取第一行第二个单元格
+print (sheet.cell(row=1,column=2).value)  #获取到第2行，第三列
 
 #获取到已经使用的行数据
-print "sheet中已经使用的行数:",sheet.max_row
+print ("sheet中已经使用的行数:",sheet.max_row)
 
 #获取sheet中已经使用到列数
-print "sheet中已经使用到的列数为：",sheet.max_column
+print ("sheet中已经使用到的列数为：",sheet.max_column)
 
 
 #获取工作表的名字
-print  "工作表的名字为：",sheet.title
+print  ("工作表的名字为：",sheet.title)
 
 
 #修改当前工作表名字
