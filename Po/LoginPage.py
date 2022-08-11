99a9bd5fef7e6201ee4df6e1d59b5c1d79d75b25
xiucaihua <1198874993@QQ.com>
2022-08-11 23:25:52 +0800

    修改原有的登录模块的定位信息

--- Po/LoginPage.py
+++ Po/LoginPage.py
@@ -10,12 +10,14 @@ log=InsertLog()
 class LoginPage(BasePage):
 
     # 元素层
-    ipt_username_loc = (By.ID, "username")                   # 登录用户名
-    ipt_password_loc = (By.ID, "password")                   # 登录密码
-    login_button_loc = (By.CSS_SELECTOR, "input.admin-btn")  # 登录按钮
-    txt_success_msg_loc = (By.XPATH, ".//*[@id='header']/p/span[1]/strong")  # 登录成功admin
-    txt_username_error_msg_loc = (By.ID, "username_msg")     # 用户名错误
-    txt_password_error_msg_loc = (By.ID, "password_msg")     # 密码错误
+  #  driver.find_element_by_name('username').send_keys('testtestYS')
+  #  driver.find_element_by_name('password').send_keys('12345678')
+
+    ipt_username_loc = (By.NAME, "username1")                                                               # 登录用户名
+    ipt_password_loc = (By.NAME, "password1")                                                               # 登录密码
+    login_button_loc = (By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div[2]/form/button[1]')         # 登录按钮
+    txt_success_msg_loc = (By.XPATH, "//span[@class='el-dropdown-link el-dropdown-selfdefine']")     # 登录成功显示testtestYS
+    txt_username_error_msg_loc = (By.XPATH, "//p[@class='el-message__content']")                        # 用户名或密码错误
 
     # 输入用户名
     def set_username(self, username):
@@ -47,21 +49,18 @@ class LoginPage(BasePage):
         except BaseException as msg:
             log.error("点击登录按钮失败！")
             self.get_screenshot_as_files(u"点击登录按钮.png")
-            print (msg)
 
 
 
     # 账号密码为空
-    def get_username_error_msg(self):
-        WebDriverWait(self.dr, 10, 0.5).until(
-            EC.text_to_be_present_in_element(self.txt_username_error_msg_loc, u"帐号或密码不能为空"))
-        r = self.dr.find_element(*self.txt_username_error_msg_loc).text
-        return r
+    # def get_username_error_msg(self):
+    #     WebDriverWait(self.dr, 10, 0.5).until(EC.text_to_be_present_in_element(self.txt_username_error_msg_loc, u"帐号或密码不能为空"))
+    #     r = self.dr.find_element(*self.txt_username_error_msg_loc).text
+    #     return r
 
-    # 密码错误
+    # 用户名或密码错误
     def get_password_error_msg(self):
-        WebDriverWait(self.dr, 10, 0.5).until(
-            EC.text_to_be_present_in_element(self.txt_password_error_msg_loc, u"密码错误"))
+        WebDriverWait(self.dr, 10, 0.5).until( EC.text_to_be_present_in_element(self.txt_password_error_msg_loc, u"用户名或密码错误"))
         r = self.dr.find_element(*self.txt_password_error_msg_loc).text
         return r
 
@@ -71,7 +70,12 @@ class LoginPage(BasePage):
         return r
 
      # 登录流程
-    def login(self, username, password):
+    def login(self, username,password):
         self.set_username(username)
         self.set_password(password)
         self.click_login_button()
+
+if __name__ == '__main__':
+    loginpage=LoginPage()
+    loginpage.open_url()
+    loginpage.login('testtestYS1','123456781')
