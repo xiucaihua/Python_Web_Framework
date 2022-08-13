b2813f9bba0bcbf770d3efa2fe810e901226f55e
xiucaihua <1198874993@QQ.com>
2022-08-13 17:11:50 +0800

    yaml文件读取操作

--- untils/operatioyaml.py
+++ untils/operatioyaml.py
@@ -1,9 +1,10 @@
 #coding:utf-8
-import json
+import yaml
 import os
 
 def base_dir():
     return os.path.dirname(os.path.dirname(__file__))
 
-def readJson():    #load()对文件进行反序列化，本质上是读取文件里面的内容
-    return json.load(open(os.path.join(base_dir(),'data','login.json')))
+def readYaml():    #load()对文件进行反序列化，本质上是读取文件里面的内容
+    with open(os.path.join(base_dir(),'data','login.yaml'),encoding='utf-8') as f:
+        return yaml.safe_load(f)
